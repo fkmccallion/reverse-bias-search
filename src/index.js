e39db42bc2e8389166956f4bc4f7a0651cb4fdf7
github username <fkmccallion@gmail.com>
2020-09-19 15:30:00 -0700

    update input style

--- src/index.js
+++ src/index.js
@@ -11,6 +11,12 @@ import * as serviceWorker from './serviceWorker';
 
 import rootReducer from './reducers/index'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+
 const store = createStore(
   rootReducer, applyMiddleware(thunk)
 );
