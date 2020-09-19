4d727fcdfee3b8a0b5f76d14a2cd060974b50be6
github username <fkmccallion@gmail.com>
2020-09-19 12:45:55 -0700

    add thunk middleware

--- src/index.js
+++ src/index.js
@@ -8,6 +8,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(
+  shoppingListItemReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
