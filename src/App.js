fd2fc33bffba8a414251b33e4fa8ca519eea58b9
github username <fkmccallion@gmail.com>
2020-09-19 13:36:13 -0700

    add query container

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Query from './containers/Query'
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        test
+        <Query />
       </header>
     </div>
   );
